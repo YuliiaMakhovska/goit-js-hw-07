5bde5c8701a8a6b87b6b5741ba8002d8729d24b6
YuliiaMakhovska <y.makhovska@gmail.com>
2022-09-22 13:41:06 +0300

    Second Task
    Second task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const refs = {
+  gallery: document.querySelector(".gallery"),
+};
 
-console.log(galleryItems);
+const imgMarkup = createImgMarkup(galleryItems);
+
+function createImgMarkup(item) {
+  return galleryItems
+    .map(({ original, preview, description }) => {
+      return `
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>`;
+    })
+    .join("");
+}
+refs.gallery.insertAdjacentHTML("beforeend", imgMarkup);
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionType: "alt",
+});
